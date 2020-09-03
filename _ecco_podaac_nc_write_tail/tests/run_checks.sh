d1b6ea9629261a6927b8ef595f307f2e9543f209
Jack McNelis <jack@deb000>
2020-09-03 11:12:26 -0400

    push updated test file CDL dumps

--- _ecco_podaac_nc_write_tail/tests/run_checks.sh
+++ _ecco_podaac_nc_write_tail/tests/run_checks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for f in /home/jack/data/tests/20200902_v2/*.nc; do 
+for f in /home/jack/data/tests/20200902_v3/*.nc; do 
     echo $f;
     name="$(basename -- $f)";
     ncdump -h $f > ${name}.cdl;
